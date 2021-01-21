5d07882c65a5f1ce1f7febd8552cc906abf82226
Ricardo Marques <ricardo@beyondpricing.com>
2021-01-21 16:05:56 +0000

    Use gin framework

--- hellavader.go
+++ hellavader.go
@@ -1,18 +1,23 @@
 package main
 
 import (
-	"io"
-	"log"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/hellavader", nowTime)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Printf("An error ocurred: %s\n", err)
-	}
-}
 
-func nowTime(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "The Force is strong with this one. Vader is commenting on the skills of an X-Wing pilot that is trying to blow up the Death Star, not realizing he’s trying to kill his own son.")
+	router := gin.Default()
+	router.Use(cors.Default())
+	// Ping test
+	router.GET("/healthz", func(c *gin.Context) {
+		c.String(http.StatusOK, "It's alive!")
+	})
+
+	router.GET("/hellavader", func(c *gin.Context) {
+		c.String(http.StatusOK, "The Force is strong with this one. Vader is commenting on the skills of an X-Wing pilot that is trying to blow up the Death Star, not realizing he’s trying to kill his own son!")
+	})
+
+	router.Run()
 }
